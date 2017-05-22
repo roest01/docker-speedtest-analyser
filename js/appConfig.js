17d71994f35345f500517db3e87b640ee4d6e689
Tobias Rös <roes@amicaldo.de>
2017-05-22 03:29:49 +0200

    fix wrong today / yesterday filter time

--- js/appConfig.js
+++ js/appConfig.js
@@ -12,8 +12,8 @@ var appConfig = {
     "startDate": moment().startOf('day'),
     "endDate": moment().endOf('day'),
     ranges: {
-      'Today': [moment(), moment()],
-      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      'Today': [moment().hours(0).minutes(0).seconds(0), moment().hours(23).minutes(59).seconds(59)],
+      'Yesterday': [moment().hours(0).minutes(0).seconds(0).subtract(1, 'days'), moment().hours(23).minutes(59).seconds(59).subtract(1, 'days')],
       'Last 7 Days': [moment().subtract(6, 'days'), moment()],
       'Last 30 Days': [moment().subtract(29, 'days'), moment()],
       'This Month': [moment().startOf('month'), moment().endOf('month')],
