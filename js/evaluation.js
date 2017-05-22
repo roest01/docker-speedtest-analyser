7ac07fd4adf78b631f7171c587b8ba57132cc302
Tobias Rös <roes@amicaldo.de>
2017-05-22 03:21:49 +0200

    refactor chart.js usage to work with v2

--- js/evaluation.js
+++ js/evaluation.js
@@ -3,60 +3,82 @@
  * @author github/roest01
  */
 jQuery(document).ready(function(){
+    var colors = {
+        orange: "rgba(255,190,142,0.5)",
+        black: "rgba(90,90,90,1)",
+        green: "rgba(143,181,178,0.8)"
+    };
     var data = {
         labels:[] ,
         datasets: [
             {
-                label: appConfig.labels.download,
-                fillColor: "rgba(255,190,142,0.5)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)"
-            },
-            {
                 label: appConfig.labels.ping,
-                fillColor: "rgba(2,2,1,0)",
-                strokeColor: "rgba(2,2,1,1)",
-                pointColor: "rgba(90,90,90,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)"
+                isMB: false,
+                fill: false,
+                backgroundColor: colors.black,
+                borderColor: colors.black,
+                tension: 0
             },
             {
                 label: appConfig.labels.upload,
-                fillColor: "rgba(143,181,178,0.8)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)"
+                isMB: true,
+                fill: true,
+                backgroundColor: colors.green,
+                borderColor: colors.green,
+                tension: 0
+            },
+            {
+                label: appConfig.labels.download,
+                isMB: true,
+                fill: true,
+                backgroundColor: colors.orange,
+                borderColor: colors.orange,
+                tension: 0
             }
         ]
     };
 
 
     var chartDom = jQuery("#speedChart").get(0).getContext("2d");
-    var chartJS = new Chart(chartDom).Line(data, {
-        responsive: true,
-        bezierCurve: false,
-        multiTooltipTemplate: "<%if (datasetLabel){%><%=datasetLabel%>: <%}%><%= value %> <%if (datasetLabel != appConfig.labels.ping){%>Mb/s<%}%>",
-        maintainAspectRatio: false
+    var chartJS = new Chart(chartDom, {
+        type: "line",
+        data: data,
+        options: {
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+                callbacks: {
+                    label: function(item, data){
+                        if (data.datasets[item.datasetIndex].isMB){
+                            return data.datasets[item.datasetIndex].label + ": "+ item.yLabel + ' Mb/s'
+                        }
+                        return data.datasets[item.datasetIndex].label + ": " + item.yLabel;
+                    }
+                }
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            responsive: true,
+            multiTooltipTemplate: "<%if (datasetLabel){%><%=datasetLabel%>: <%}%><%= value %> <%if (datasetLabel != appConfig.labels.ping){%>Mb/s<%}%>"
+            //maintainAspectRatio: false
+        }
     });
 
 
     var ParseManager = function(){
-        this.header = null;
-        this._startDate = null;
-        this._endDate = null;
-        this._chart = null;
-        this.i = 0;
+        var parseManager = this;
+        parseManager.header = null;
+        parseManager._startDate = null;
+        parseManager._endDate = null;
+        parseManager._chart = null;
+        parseManager.i = 0;
 
         /**
          * parse result.csv and create graph with _startDate and _endDate filter
          */
-        this.parse = function(){
+        parseManager.parse = function(){
             var parseManager = this;
             parseManager.i = 0;
 
@@ -65,7 +87,7 @@ jQuery(document).ready(function(){
                 step: function(row) { //using stream to allow huge file progressing
                     parseManager.i++;
                     var dataArr = row.data[0];
-                    if (!parseManager.header || parseManager.i == 1){
+                    if (!parseManager.header || parseManager.i === 1){
                         parseManager.header = dataArr;
                     } else {
                         //build csv array
@@ -76,9 +98,9 @@ jQuery(document).ready(function(){
                         measureRow['timestamp_s'] = parseInt(measureRow['timestamp'] / 1000); //from ms timestamp to secounds
                         measureRow['timestamp'] = parseInt(measureRow['timestamp']); //from save ms timestamp
 
-                        if (parseManager._startDate != null && parseManager._endDate != null){
+                        if (!!parseManager._startDate && !!parseManager._endDate){
                             if (measureRow['timestamp_s'] < parseManager._startDate.unix() || measureRow['timestamp_s'] > parseManager._endDate.unix()){
-                                //parseManager.removeRow(measureRow);
+                                //not in filter
                                 return;
                             }
                         }
@@ -94,43 +116,41 @@ jQuery(document).ready(function(){
          *
          * @param measureRow
          */
-        this.addRow = function(measureRow){
-            this._chart.addData([
-                measureRow['download'],
-                measureRow['ping'],
+        parseManager.addRow = function(measureRow){
+            var chart = parseManager._chart;
+            var chartData = chart.config.data;
+            chartData.labels.push(this.getDateFromData(measureRow));
+
+            chartData.datasets[0].data.push(
+                measureRow['ping']
+            );
+            chartData.datasets[1].data.push(
                 measureRow['upload']
-            ], this.getDateFromData(measureRow));
+            );
+            chartData.datasets[2].data.push(
+                measureRow['download']
+            );
+
+            parseManager._chart.config.data = chartData;
+            chart.update();
         };
 
-        this.flushChart = function(force, callback){
+        parseManager.flushChart = function(force, callback){
             var parseManager = this;
-            var points = this._chart.datasets[0].points;
+            var chart = parseManager._chart;
 
-            for (i = 0; i <  points.length; i++) {
-                var currentDatapointTime = moment(points[i]['label'], 'L - LT').unix();
-                if (currentDatapointTime < parseManager._startDate.unix() || force){
-                    setTimeout(function(){
-                        parseManager._chart.removeData();
+            chart.data.labels = [];
+            chart.data.datasets.forEach(function(dataSet){
+                dataSet.data = [];
+            });
 
-                        if (points.length == 0){
-                            callback();
-                        }
-                    }, i*10); //timeout need by chart.js :(
-                } else if (currentDatapointTime > parseManager._endDate.unix()) {
-                    //@todo removeData with index is not supported by Chart.js
-                    jQuery.each(this._chart.datasets, function(index, singleDataset){
-                        //delete singleDataset.points[i]; <- this does not work
-                        //parseManager._chart.update();
-
-                        parseManager.flushChart(true);//dirty solution (not in use)
-                    });
-                    break;
-                }
-            }
+            parseManager._chart.update();
+            callback();
+            return true;
         };
 
 
-        this.getDateFromData = function(measureRow){
+        parseManager.getDateFromData = function(measureRow){
             return moment(new Date(measureRow['timestamp'])).format('L - LT')
         };
 
@@ -141,9 +161,9 @@ jQuery(document).ready(function(){
          * @param startDate
          * @returns {ParseManager}
          */
-        this.setStartDate = function(startDate){
-            this._startDate = startDate;
-            return this;
+        parseManager.setStartDate = function(startDate){
+            parseManager._startDate = startDate;
+            return parseManager;
         };
 
         /**
@@ -152,9 +172,9 @@ jQuery(document).ready(function(){
          * @param endDate
          * @returns {ParseManager}
          */
-        this.setEndDate = function(endDate){
-            this._endDate = endDate;
-            return this;
+        parseManager.setEndDate = function(endDate){
+            parseManager._endDate = endDate;
+            return parseManager;
         };
 
        /**
@@ -195,22 +215,23 @@ jQuery(document).ready(function(){
     jQuery.extend(daterangeConfig, appConfig.daterange);
 
     //init application
+    jQuery(document).ready(function(){
+        var parseManager = new ParseManager();
+        parseManager.setChart(chartJS);
+        var dateRange = jQuery('input[name="daterange"]');
+        dateRange.daterangepicker(
+            daterangeConfig,
+            function(start, end) {
+                parseManager.update(start,end);
+            });
 
-    var parseManager = new ParseManager();
-    parseManager.setChart(chartJS);
-    var dateRange = jQuery('input[name="daterange"]');
-    dateRange.daterangepicker(
-        daterangeConfig,
-        function(start, end) {
-            parseManager.update(start,end);
-        });
+        moment.locale(appConfig.locale);
 
-    moment.locale(appConfig.locale);
-
-    if (appConfig.daterange.startDate && appConfig.daterange.endDate){
-        parseManager
-            .setStartDate(appConfig.daterange.startDate)
-            .setEndDate(appConfig.daterange.endDate);
-    }
-    parseManager.parse();
+        if (appConfig.daterange.startDate && appConfig.daterange.endDate){
+            parseManager
+                .setStartDate(appConfig.daterange.startDate)
+                .setEndDate(appConfig.daterange.endDate);
+        }
+        parseManager.parse();
+    });
 });
