c48718e4c5f8485f6685fab7b5d1588ae7ba96e8
Daniel Schischkin <daniel.schischkin@surplex.com>
2019-10-27 06:53:41 +0100

    [WIP] Fix error in run.sh

--- config/run.sh
+++ config/run.sh
@@ -1,17 +1,14 @@
-#!/usr/bin/env bash
+#!sh
 echo "Starting run.sh"
 
 cat /var/www/html/config/crontab.default > /var/www/html/config/crontab
 
-if [[ ${CRONJOB_ITERATION} && ${CRONJOB_ITERATION-x} ]]; then
+if [[ ${CRONJOB_ITERATION} ]]; then
     sed -i -e "s/0/*\/${CRONJOB_ITERATION}/g" /var/www/html/config/crontab
 fi
 crontab /var/www/html/config/crontab
 
 echo "Starting Cronjob"
 crond -l 2 -f &
-
-echo "Starting nginx"
-exec nginx -g "daemon off;"
 
 exit 0;
